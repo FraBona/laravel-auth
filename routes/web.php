179823ef766e45b4091af883dbea7670ab0b9d81
FraBona <francescobonafede08@gmail.com>
2023-12-21 15:55:59 +0100

    create model migration seeder and controller

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function(){
     Route::get('/', function () {
-        return view('dashboard');
+        return view('admin.dashboard');
     })->name('dashboard');
 });
 
